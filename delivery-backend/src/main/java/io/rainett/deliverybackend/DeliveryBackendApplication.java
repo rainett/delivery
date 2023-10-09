11422ef913437421f7971e892c70120fe1c5b5e9
rainett <96131229+rainett@users.noreply.github.com>
2023-10-09 22:52:54 +0300

    initial commit

---
+++ delivery-backend/src/main/java/io/rainett/deliverybackend/DeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package io.rainett.deliverybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryBackendApplication.class, args);
+    }
+
+}
---
+++ delivery-backend/src/test/java/io/rainett/deliverybackend/DeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rainett.deliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
